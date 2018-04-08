1f7c859eef4659b8fa562479ec7b6512afc05f83
Mikael Blomstrand <mbloms@kth.se>
2018-04-09 00:40:37 +0200

    comment out failing code

--- compiler/src/dotty/tools/dotc/transform/MkPhantom.scala
+++ compiler/src/dotty/tools/dotc/transform/MkPhantom.scala
@@ -13,7 +13,7 @@ class MkPhantom extends MiniPhase {
 
   override def phaseName: String = "MkPhantom"
 
-  override def transformStats(trees: List[tpd.Tree])(implicit ctx: Contexts.Context): List[tpd.Tree] = {
+  /*override def transformStats(trees: List[tpd.Tree])(implicit ctx: Contexts.Context): List[tpd.Tree] = {
     for (declaration <- trees;
          typeDef <- (declaration match {
            case TypeDef(name, tree) => List(TypeDef(name, tree), TypeDef((name ++ "Phantom").toTypeName, tree))
@@ -29,5 +29,5 @@ class MkPhantom extends MiniPhase {
       //tree.tpe
       TypeDef(newName, Template(null, originalType :: Nil))
     }
-  }
+  }*/
 }

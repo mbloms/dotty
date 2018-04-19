599d234f4b1f8c07e86af9ae3174609482939f62
Mikael Blomstrand <mbloms@kth.se>
2018-04-19 15:15:51 +0200

    Hiding everything away in compaion object.

--- phantom-test/Inner.scala
+++ phantom-test/Inner.scala
@@ -1,23 +1,23 @@
-case class Fordon(val maxSpeed: Int) {
+class Fordon(val maxSpeed: Int) {
     def this() = this(0)
 
-    def faster = PhantomFordon(maxSpeed+1)
+    def faster = Fordon(maxSpeed+1)
 }
 
-trait PhantomFordon extends Fordon
 
-object PhantomFordon {
-    private class PhantomFordon1(maxSpeed: Int) extends Fordon(maxSpeed) with PhantomFordon
-    private class PhantomFordon2 extends Fordon() with PhantomFordon
-    def apply(): PhantomFordon = new PhantomFordon2
-    def apply(i: Int): PhantomFordon = new PhantomFordon1(i)
+object Fordon {
+    trait Phantom extends Fordon
+    private class PhantomFordon1(maxSpeed: Int) extends Fordon(maxSpeed) with Phantom
+    private class PhantomFordon2 extends Fordon() with Phantom
+    def apply(): Phantom = new PhantomFordon2
+    def apply(i: Int): Phantom = new PhantomFordon1(i)
 }
 
 
 object Inner extends App {
     val f: Fordon = new Fordon
-    val f1: Fordon = PhantomFordon(2)
-    val f2: Fordon = PhantomFordon()
+    val f1: Fordon = Fordon(2)
+    val f2: Fordon = Fordon()
     val f3: Fordon = new PhantomBil1(3)
     //val f: Fordon = new Fordon
     println(f)
@@ -29,6 +29,6 @@ object Inner extends App {
 
 class Bil(maxSpeed: Int) extends Fordon(maxSpeed)
 
-trait PhantomBil extends Bil with PhantomFordon
+trait PhantomBil extends Bil with Fordon.Phantom
 
 class PhantomBil1(maxSpeed: Int) extends Bil(maxSpeed) with PhantomBil

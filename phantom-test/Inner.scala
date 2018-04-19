77e413289bae8faf6453891e60286691820fc2c5
Mikael Blomstrand <mbloms@kth.se>
2018-04-19 15:05:17 +0200

    Phantom classes private in object

--- phantom-test/Inner.scala
+++ phantom-test/Inner.scala
@@ -1,18 +1,23 @@
 case class Fordon(val maxSpeed: Int) {
     def this() = this(0)
 
-    def faster = new PhantomFordon1(maxSpeed+1)
+    def faster = PhantomFordon(maxSpeed+1)
 }
 
 trait PhantomFordon extends Fordon
 
-class PhantomFordon1(maxSpeed: Int) extends Fordon(maxSpeed) with PhantomFordon
-class PhantomFordon2 extends Fordon() with PhantomFordon
+object PhantomFordon {
+    private class PhantomFordon1(maxSpeed: Int) extends Fordon(maxSpeed) with PhantomFordon
+    private class PhantomFordon2 extends Fordon() with PhantomFordon
+    def apply(): PhantomFordon = new PhantomFordon2
+    def apply(i: Int): PhantomFordon = new PhantomFordon1(i)
+}
+
 
 object Inner extends App {
     val f: Fordon = new Fordon
-    val f1: Fordon = new PhantomFordon1(2)
-    val f2: Fordon = new PhantomFordon2
+    val f1: Fordon = PhantomFordon(2)
+    val f2: Fordon = PhantomFordon()
     val f3: Fordon = new PhantomBil1(3)
     //val f: Fordon = new Fordon
     println(f)

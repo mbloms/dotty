2c96a02b735948f9ec18679c34e4cd8b934faae7
Mikael Blomstrand <mbloms@kth.se>
2018-05-03 13:12:27 +0200

    buu

--- phantom-test/Tast.scala
+++ phantom-test/Tast.scala
@@ -3,7 +3,8 @@ class Bil(val x: Int)
 class Volvo extends Bil(1)
 final class Saab(i: Int) extends Bil(i)
 
-object Main extends App {
-    val b: Bil = new Bil(4) {}
+object Main {
+    val b: Bil = Bil.newPhantom(4)
     println(b)
+    println(new Bil(2))
 }
